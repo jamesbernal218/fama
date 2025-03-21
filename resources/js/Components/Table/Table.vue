07090f7c9c6ce4a3964fb933b4dda2c45d5cb983
Jaime Bernal <jaimejair.bernal@corporativopmn.com>
2025-03-21 01:02:49 -0600

    creacion de componentes

---
+++ resources/js/Components/Table/Table.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg px-20">
+    <table
+      class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+    >
+      <thead
+        class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+      >
+        <tr>
+          <th v-for="column in columns" :key="column.key" class="px-6 py-3">
+            {{ column.label }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in data"
+          :key="item.id"
+          class="odd:bg-white even:bg-gray-50 dark:odd:bg-gray-900 dark:even:bg-gray-800 border-b dark:border-gray-700 border-gray-200"
+        >
+          <td v-for="column in columns" :key="column.key" class="px-6 py-4">
+            {{ item[column.key] }}
+          </td>
+
+          <td class="px-6 py-4 flex gap-2">
+            <a
+              :href="`${routeEdit}/${item.id}`"
+              class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            >
+              Editar
+            </a>
+            <a
+              :href="`${routeDelete}/${item.id}`"
+              class="font-medium text-red-600 dark:text-red-500 hover:underline"
+            >
+              Eliminar
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    columns: {
+      type: Array,
+      required: true,
+    },
+    data: {
+      type: Array,
+      required: true,
+    },
+    routeEdit: {
+      type: String,
+      required: true,
+    },
+    routeDelete: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ resources/js/Pages/Medicine/Medicine.vue
@@ -0,0 +1,60 @@
+<template>
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="text-xl font-semibold leading-tight text-gray-800">Medicinas</h2>
+    </template>
+    <div class="p-6">
+      <h1 class="text-2xl font-bold mb-4">Medicines</h1>
+      <Table
+        :columns="columns"
+        :data="info.data"
+        :routeEdit="'/medicine/edit'"
+        :routeDelete="'/medicine/delete'"
+      />
+
+      <div class="mt-4 flex justify-between">
+        <button
+          @click="changePage(info.prev_page_url)"
+          :disabled="!info.prev_page_url"
+          class="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
+        >
+          Previous
+        </button>
+        <button
+          @click="changePage(info.next_page_url)"
+          :disabled="!info.next_page_url"
+          class="px-4 py-2 bg-blue-500 text-white rounded disabled:opacity-50"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  </AuthenticatedLayout>
+</template>
+
+<script>
+import Table from "@/Components/Table/Table.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+
+export default {
+  props: {
+    info: Object,
+    paginate: Number,
+  },
+  components: {
+    Table,
+    AuthenticatedLayout,
+  },
+  data() {
+    return {
+      columns: [
+        { label: "Nombre", key: "name" },
+        { label: "Precio", key: "price" },
+        { label: "Categoría", key: "category_id" },
+        { label: "Provedor", key: "supplier_id" },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
