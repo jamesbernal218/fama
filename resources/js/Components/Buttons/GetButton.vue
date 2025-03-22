aaa33991cabb85820081aea3596c0f669a1c8f93
Jaime Bernal <jaimejair.bernal@corporativopmn.com>
2025-03-21 18:04:13 -0600

    diseño table

--- resources/js/Components/Buttons/GetButton.vue
+++ resources/js/Components/Buttons/GetButton.vue
@@ -3,7 +3,7 @@
     :disabled="isLoading"
     @click="submit"
     type="button"
-    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    class="text-white bg-green-600 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
   >
     <svg
       xmlns="http://www.w3.org/2000/svg"
--- resources/js/Components/Table/Table.vue
+++ resources/js/Components/Table/Table.vue
@@ -1,32 +1,37 @@
 <template>
-  <div class="relative overflow-x-auto shadow-md sm:rounded-lg px-20">
-    <table
-      class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-    >
+  <div
+    class="relative overflow-x-auto shadow-lg rounded-lg border border-gray-200 dark:border-gray-700"
+  >
+    <table class="w-full text-sm text-left text-gray-800 dark:text-gray-200">
+      <!-- Encabezado -->
       <thead
-        class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+        class="text-xs uppercase bg-green-600 text-white dark:bg-gray-800 dark:text-gray-300"
       >
         <tr>
-          <th v-for="column in columns" :key="column.key" class="px-6 py-3">
+          <th v-for="column in columns" :key="column.key" class="px-6 py-4 text-center">
             {{ column.label }}
           </th>
+          <th v-if="routeEdit || routeDelete" class="px-6 py-4 text-center">Acciones</th>
         </tr>
       </thead>
+
+      <!-- Cuerpo de la tabla -->
       <tbody>
         <tr
           v-for="item in data"
           :key="item.id"
-          class="odd:bg-white even:bg-gray-50 dark:odd:bg-gray-900 dark:even:bg-gray-800 border-b dark:border-gray-700 border-gray-200"
+          class="odd:bg-green-50 even:bg-white dark:odd:bg-gray-900 dark:even:bg-gray-800 border-b dark:border-gray-700 hover:bg-green-100 dark:hover:bg-gray-700 transition"
         >
-          <td v-for="column in columns" :key="column.key" class="px-6 py-4">
+          <td v-for="column in columns" :key="column.key" class="px-6 py-4 text-center">
             {{ item[column.key] }}
           </td>
 
-          <td class="px-6 py-4 flex gap-2">
+          <!-- Acciones -->
+          <td v-if="routeEdit || routeDelete" class="px-6 py-4 flex justify-center gap-4">
             <a
               v-if="routeEdit"
               :href="`${routeEdit}/${item.id}`"
-              class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              class="px-4 py-2 text-sm font-medium text-white bg-green-500 rounded-md hover:bg-green-700 transition"
             >
               Editar
             </a>
@@ -34,7 +39,7 @@
             <a
               v-if="routeDelete"
               :href="`${routeDelete}/${item.id}`"
-              class="font-medium text-red-600 dark:text-red-500 hover:underline"
+              class="px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-md hover:bg-red-700 transition"
             >
               Eliminar
             </a>
