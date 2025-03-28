c41509aeb47f75f8c2a1450406a7029ce757c7dd
jair <jairaaron11@gmail.com>
2025-03-27 21:15:50 -0600

    add

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/css/app.css', // Incluye tu archivo CSS
+                'resources/js/app.js',   // Tu archivo JS principal
+            ],
             refresh: true,
         }),
         vue({
@@ -17,4 +20,13 @@ export default defineConfig({
             },
         }),
     ],
+    build: {
+        manifest: true,  // Asegura que se genere el archivo manifest.json
+        outDir: 'public/build', // Directorio de salida para los archivos compilados
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+            },
+        },
+    },
 });
