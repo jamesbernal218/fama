802aa2563579377c93f6adab69dd6e6c1c71f440
jair <jairaaron11@gmail.com>
2025-03-28 12:09:16 -0600

    add

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'; // Importar el plugin de Vue
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/js/app.js'],
-            refresh: true,
-        }),
-        vue(), // Agregar el plugin de Vue
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
-    build: {
-        manifest: true, 
-        outDir: 'public/build', 
-        emptyOutDir: true,
-    }
 });
