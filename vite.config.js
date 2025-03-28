bbfda562191eaaebc509c8f2bcab8f6287c10b4e
jair <jairaaron11@gmail.com>
2025-03-27 21:33:22 -0600

    add

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css', // Incluye tu archivo CSS
-                'resources/js/app.js',   // Tu archivo JS principal
-            ],
+            input: ['resources/js/app.js'],
             refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
         }),
     ],
     build: {
-        manifest: true,  // Asegura que se genere el archivo manifest.json
-        outDir: 'public/build', // Directorio de salida para los archivos compilados
-        rollupOptions: {
-            input: {
-                app: 'resources/js/app.js',
-            },
-        },
-    },
+        manifest: true, // Asegura que se genere el manifest.json
+        outDir: 'public/build', // Cambia la carpeta de salida
+        emptyOutDir: true, // Limpia la carpeta antes de construir
+    }
 });
